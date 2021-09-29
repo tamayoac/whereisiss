2792bcf696f4728e567183aa0732dd7bcae379e7
Alfred <red.yamato@gmail.com>
2021-09-30 01:50:10 +0800

    update angular json

--- src/app/components/mapiss/mapiss.component.ts
+++ src/app/components/mapiss/mapiss.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
 import * as L from 'leaflet';
-
 const iconRetinaUrl = 'assets/marker-icon-2x.png';
 const iconUrl = 'assets/marker-icon.png';
 const shadowUrl = 'assets/marker-shadow.png';
@@ -27,26 +26,33 @@ export class MapissComponent implements OnInit {
   @Input() longitude!: any;
   private map!: L.Map;
   private marker!: L.Marker;
+  private myIcon! :L.Icon;
   // private centeroid: L.LatLngExpression = [this.latitude ,this.longitude];
 
  
   private initMap(): void {
   
     this.map = L.map('map').setView([0,0], 6);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+
+    this.myIcon = L.icon({
+      iconUrl: '../../../assets/images/international-space-station-icon.png',
+      iconSize: [60, 42],
+      iconAnchor: [25, 16],
+      popupAnchor: [-3, -76],
+    });
+    L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
       maxZoom: 10,
       noWrap: true,
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
     }).addTo(this.map);
 
-    this.marker = L.marker([0,0]).addTo(this.map)
+    this.marker = L.marker([0,0], {icon: this.myIcon }).addTo(this.map)
   }
 
   private updateLocation(lat: any, long: any): void{
     
     this.marker.setLatLng([lat, long]);
-  
-      this.map.setView([lat, long], this.map.getZoom());
+    this.map.setView([lat, long], this.map.getZoom());
   }
   ngOnInit(): void {
    
