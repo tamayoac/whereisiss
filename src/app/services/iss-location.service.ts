023e98e97365d43e617300c7220c726f640dea46
Alfred Christopher <red.yamato@gmail.com>
2024-08-15 03:57:17 +0800

    add more logger

--- src/app/services/iss-location.service.ts
+++ src/app/services/iss-location.service.ts
@@ -10,9 +10,12 @@ import { environment } from '../../environments/environment';
 export class ISSLocationService {
   private apiUrl = environment.issapi
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    console.log('ISS API URL:', this.apiUrl);  
+  }
 
   getLocation(): Observable<ISSLocation> {
+    console.log(this.apiUrl);
     return this.http.get<ISSLocation>(this.apiUrl);
   }
 }
